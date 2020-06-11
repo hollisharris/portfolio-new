0233a513501623caabb36f17d496e858399f8bb1
Work <work@Holliss-MBP.lan>
2020-06-11 14:13:13 -0500

    added dummy image in images folder

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -3,7 +3,6 @@ require("dotenv").config({
 })
 
 module.exports = {
-  pathPrefix: `/frontend`,
   siteMetadata: {
     title: `Hollis`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
